7f0497ec3b232d4da5999798cf3188ee5a8b2f8c
thedavidkipnis <thedavidkipnis@gmail.com>
2023-11-24 14:53:10 -0800

    Modified script logic to generate grid

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-let grid = [];
+//let grid = [];
 
 function genGrid(rows, cols) {
-    var body = document.body;
 
-    for (var i = 0; i < rows; i++) {
+    let screenHeight = screen.height;
+    let screenWidth = screen.width;
+
+    for (var i = 0; i < Math.floor(screenHeight/100); i++) {
         var row = document.createElement("div");
-        row.className = "grid-container";
+        row.className = "grid-row";
 
-        let gridRow = [];
-        for (var j = 0; j < cols; j++) {
+        //let gridRow = [];
+        for (var j = 0; j < Math.floor(screenWidth/100); j++) {
             var cell = document.createElement("div");
-            cell.className = "grid-container";
+            cell.className = "grid-cell";
             row.appendChild(cell);
 
-            gridRow[j] = cell;
+            //gridRow[j] = cell;
         }
-        body.appendChild(row);
-        grid[i] = gridRow;
+
+        document.getElementById("grid-container").appendChild(row);
+        //grid[i] = gridRow;
     }
 
-    document.getElementById("code").innerText = body.innerHTML;
+    // var row = document.createElement("div");
+    // row.className = "grid-container";
+    // row.innerHTML = "ADDED";
+    // document.getElementById("grid-container").appendChild(row);
 }
