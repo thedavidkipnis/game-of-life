c806a0c3ddaa868212a039e0e494fffc169db12c
David Kipnis <thedavidkipnis@gmail.com>
2023-11-26 21:38:47 -0800

    Made grid cover most of page - added automatic grid updating

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-//let grid = [];
+var grid = [];
+
+function main() {
+    genGrid(3,3);
+    setInterval(switchGrid, 500);
+}
 
 function genGrid(rows, cols) {
 
     let screenHeight = screen.height;
     let screenWidth = screen.width;
 
-    for (var i = 0; i < Math.floor(screenHeight/100); i++) {
+    for (var i = 0; i < Math.floor(screenHeight/35); i++) {
         var row = document.createElement("div");
         row.className = "grid-row";
 
-        //let gridRow = [];
-        for (var j = 0; j < Math.floor(screenWidth/100); j++) {
+        var gridRow = [];
+        for (var j = 0; j < Math.floor(screenWidth/35); j++) {
             var cell = document.createElement("div");
-            cell.className = "grid-cell";
+
+            cell.className = "grid-cell-filled";
+
             row.appendChild(cell);
 
-            //gridRow[j] = cell;
+            gridRow[j] = cell;
         }
 
         document.getElementById("grid-container").appendChild(row);
-        //grid[i] = gridRow;
+        grid[i] = gridRow;
     }
+}
 
-    // var row = document.createElement("div");
-    // row.className = "grid-container";
-    // row.innerHTML = "ADDED";
-    // document.getElementById("grid-container").appendChild(row);
+function switchGrid() {
+    console.log("heyyy");
+    grid.forEach(row => {
+        row.forEach(cell => {
+            if(Math.floor(Math.random() * 2) == 1) {
+                cell.className = "grid-cell-unfilled";
+            } else {
+                cell.className = "grid-cell-filled";
+            }
+        })
+    });
 }
